4c80e1421d4587a4eb4d5a5ed282626e875870c8
dj antivaxx <dj.antivaxx@gmail.com>
2024-02-19 17:33:42 -0500

    set up da wtforms gig

--- src/blueprints/home.py
+++ src/blueprints/home.py
@@ -1,39 +1,37 @@
-from flask import Blueprint, render_template, request, url_for, flash, redirect
+from flask import Blueprint, render_template, request, url_for, redirect
 from database.database import fetch_all_threads_and_posts, add_post
 from utils.data_parsers import parse_content, parse_request_images
 from utils.data_rendering import render_all_posts, order_threads_by_recent
+from wtforms import Form, StringField, SubmitField, validators
 
 home = Blueprint('home', __name__)
 
+class NewThreadForm(Form):
+    title = StringField('title', validators=[validators.InputRequired(message="Title is empty!"), validators.Length(min=4, max=15, message="Title is too long or too short!")])
+    content = StringField('content', validators=[validators.DataRequired(), validators.Length(min=4, max=15, message="Content is too long or too short!")])
+    submit = SubmitField('post')
 
 @home.route('/', methods=('GET', 'POST'))
 def index():
-    if request.method == 'POST':
+    form = NewThreadForm(request.form)
+    
+    if request.method == 'POST' and form.validate():
 
-        content = parse_content(request.form['content'])
-        title = request.form['title']
+        content = parse_content(form.content.data)
+        title = form.title.data
         images = parse_request_images(request.files.getlist('image'))
-
-        if content is None:
-            flash('Content is required!')
-            return redirect(url_for('home.index'))
-        
-        if title is None:
-            flash('Title is required!')
-            return redirect(url_for('home.index'))
         
         new_thread = True
         sender_ip = None
         thread_id = None
         add_post(new_thread, content, sender_ip, images, thread_id, title)
-  
         return redirect(url_for('home.index'))
-    
+
     threads_and_posts_data = fetch_all_threads_and_posts()
     threads_and_posts_data = order_threads_by_recent(threads_and_posts_data)
 
     posts_data = [data['posts_data'] for data in threads_and_posts_data]
     threads_data = [data['thread_data'] for data in threads_and_posts_data]
-    rendered_posts_data = render_all_posts(posts_data)
-
-    return render_template('index.html', threads=threads_data, posts=rendered_posts_data)
+    rendered_posts_data = render_all_posts(posts_data)        
+        
+    return render_template('index.html', threads=threads_data, posts=rendered_posts_data, form=form)
--- src/blueprints/thread.py
+++ src/blueprints/thread.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, render_template, request, url_for, flash, redirect, send_from_directory
+from flask import Blueprint, render_template, request, send_from_directory, url_for, redirect
 from database.database import get_posts_by_thread_id, get_thread_data_by_thread_id, add_post
 from utils.data_parsers import parse_content, parse_request_images
 from utils.data_rendering import render_thread_posts
+from wtforms import Form, StringField, SubmitField, validators
 
 from globals import RELATIVE_UPLOADS_DIR
 
 thread_bp = Blueprint('thread', __name__)
+
+class NewPost(Form):
+    content = StringField('content', validators=[validators.DataRequired(), validators.Length(min=4, max=15, message="Content is too long or too short!")])
+    submit = SubmitField('post')
 
 @thread_bp.route('/uploads/<filename>')
 def upload(filename):
@@ -13,24 +18,22 @@ def upload(filename):
 
 @thread_bp.route('/thread/<thread_id>', methods=['GET', 'POST'])
 def thread(thread_id):
-    if request.method == 'POST':
-        content = parse_content(request.form['content'])
-        images = parse_request_images(request.files.getlist('image'))
+    form = NewPost(request.form)
+    if request.method == 'POST' and form.validate():
 
-        if content is None:
-            flash('Content is required!')
-            return redirect(url_for('thread.thread', thread_id=thread_id))
+        content = parse_content(form.content.data)
+        images = parse_request_images(request.files.getlist('image'))
 
         new_thread = False
         sender_ip = None
         title = None
         add_post(new_thread, content, sender_ip, images, thread_id, title)
-
         return redirect(url_for('thread.thread', thread_id=thread_id))
     
     thread_data = get_thread_data_by_thread_id(thread_id)
     thread_posts = get_posts_by_thread_id(thread_id)
     rendered_thread_posts = render_thread_posts(thread_posts)
+        
     
-    return render_template("thread.html", posts=rendered_thread_posts, thread=thread_data)
+    return render_template("thread.html", posts=rendered_thread_posts, thread=thread_data, form=form)
 
